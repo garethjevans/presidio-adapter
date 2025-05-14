c733068e0e149cd0d013faaeb9c57f96fe7dc726
Gareth Evans <gareth@bryncynfelin.co.uk>
2025-05-14 11:04:35 +0100

    fix: correct compilation errors

--- src/main/java/com/garethjevans/ai/presidio/ChatController.java
+++ src/main/java/com/garethjevans/ai/presidio/ChatController.java
@@ -52,7 +52,8 @@ public class ChatController {
                     request.toolChoice(),
                     request.parallelToolCalls(),
                     request.user(),
-                    request.reasoningEffort()));
+                    request.reasoningEffort(),
+                    request.webSearchOptions()));
         } catch (BannedElementException exp) {
             return new ResponseEntity<String>(exp.getMessage(), HttpStatus.BAD_REQUEST);
         }
